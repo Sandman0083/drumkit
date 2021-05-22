cec56cdf4f6c768fcb0eaa0d7c6a859151680f68
Sandman0083 <82945200+Sandman0083@users.noreply.github.com>
2021-05-22 13:06:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function makeSound(key) {
 
   switch (key) {
     case "w":
-      var tom1 = new Audio("sounds/tom-1.mp3");
+      var tom1 = new Audio("sounds/crash.mp3");
       tom1.play();
       break;
 
@@ -51,7 +51,7 @@ function makeSound(key) {
       break;
 
     case "k":
-      var crash = new Audio('sounds/crash.mp3');
+      var crash = new Audio('sounds/tom-1.mp3');
       crash.play();
       break;
 
